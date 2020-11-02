23f7c4b8bf629efccc6a25a397bc4df0e7c31298
Sebastián Sosa <velstadtsosa@gmail.com>
2020-11-01 20:29:11 -0600

    Finished Show Date SP

--- Second Unity/Various Practices/sp_showDate.sql
+++ Second Unity/Various Practices/sp_showDate.sql
@@ -16,41 +16,41 @@ DELIMITER $$
 
     DROP PROCEDURE IF EXISTS sp_showDate$$
 
-    CREATE PROCEDURE sp_showDate(IN D INT, IN M INT, IN Y INT, OUT dat VARCHAR(50))
+    CREATE PROCEDURE sp_showDate(IN D INT, IN M INT, IN Y INT)
 
     BEGIN
         
-        DECLARE month_name VARCHAR(15) DEFAULT 'NONE';
-
-        DECLARE countr INT DEFAULT 0;
-        DECLARE leap TINYINT DEFAULT 0;
-        DECLARE i INT DEFAULT 0;
+        SET @month_name = 'NONE';
+        
+        SET @countr = 0;        
+        SET @leap = 0;        
+        SET @i = 0;
 
         -- Let's see if it is a leap year.
         REPEAT
-            IF countr = Y THEN
-                SET leap = 1;
+            IF @countr = Y THEN
+                SET @leap = 1;
             ELSE
-                SET countr = countr + 4;
+                SET @countr = @countr + 4;
+            END IF;
         
-        SET i = i + 1;
-        UNTIL i < 2501
-        END REPEAT;
+        SET @i = @i + 1;
+        UNTIL @i = 2500 END REPEAT;
 
-        IF Y > 0 AND Y < 10001 THEN
-            IF M > 0 AND M < 13 THEN
-                IF D > 0 AND D < 32 THEN
+        IF Y BETWEEN 0 AND 10000 THEN
+            IF M BETWEEN 1 AND 12 THEN
+                IF D BETWEEN 1 AND 31 THEN
                     IF M IN (1, 3, 5, 7, 8, 10, 12) THEN
-                        SET month_name = (SELECT var_month FROM Months WHERE id = M);
+                        SET @month_name = (SELECT var_month FROM Months WHERE id = M);
                     ELSEIF D > 0 AND D < 31 THEN
                         IF M IN (4, 6, 9, 11) THEN
-                            SET month_name = (SELECT var_month FROM Months WHERE id = M);
+                            SET @month_name = (SELECT var_month FROM Months WHERE id = M);
                         END IF;
                     ELSEIF D > 0 AND D < 30 THEN
                             IF D < 29 THEN 
-                                SET month_name = (SELECT var_month FROM Months WHERE id = 2);
+                                SET @month_name = (SELECT var_month FROM Months WHERE id = 2);
                             ELSEIF D = 29 THEN 
-                                SET month_name = (SELECT var_month FROM Months WHERE id = 2);
+                                SET @month_name = (SELECT var_month FROM Months WHERE id = 2);
                             ELSE
                                 SELECT 'Not valid date' AS 'Error';
                             END IF;
@@ -61,18 +61,18 @@ DELIMITER $$
             END IF;
         END IF;
 
-        IF leap = 1 THEN
-            IF month_name = "NONE" THEN
+        IF @leap = 1 THEN
+            IF @month_name = "NONE" THEN
                 SELECT 'Not valid date' AS 'Error';
             ELSE
-                SELECT CONCAT('¡Es año bisiesto! Es ', D, ' de ', month_name, ' de ', Y);
+                SELECT CONCAT('¡Es año bisiesto! Es ', D, ' de ', @month_name, ' de ', Y) AS "Resultado";
             END IF;
         
         ELSE
-            IF month_name = "NONE" THEN
+            IF @month_name = "NONE" THEN
                 SELECT 'Not valid date' AS 'Error';
             ELSE
-                SELECT CONCAT('Es ', D, ' de ', month_name, ' de ', Y);
+                SELECT CONCAT('Es ', D, ' de ', @month_name, ' de ', Y) AS "Resultado";
             END IF;
         
         END IF;
@@ -81,7 +81,4 @@ DELIMITER $$
 
 DELIMITER ;
 
-
-
-
-
+CALL sp_showDate(19, 11, 2020);
