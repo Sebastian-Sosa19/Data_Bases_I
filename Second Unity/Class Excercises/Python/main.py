004900075448b5272d4947a98fc090583a7ef0dd
Sebastián Sosa <velstadtsosa@gmail.com>
2020-12-03 12:11:16 -0600

    Testing file generator

--- Second Unity/Class Excercises/Python/main.py
+++ Second Unity/Class Excercises/Python/main.py
@@ -6,11 +6,14 @@
 
 from ConnectionConfig import ConnectionConfig
 from MySQLEngine import MySQLEngine
+import dataGenerator
 
+"""
 config = ConnectionConfig("localhost","3306","root","lockandgo","Example")
 engine = MySQLEngine(config)
 first10 = engine.select("SELECT * FROM Measure ORDER BY id ASC LIMIT 10")
 last10 = engine.select("SELECT * FROM Measure ORDER BY id DESC LIMIT 10")
+"""
 
 # for id,device,temperature,date in first10:
 #     print("Registro: %s, %s, %s, %s" % (id,device,temperature,date))
@@ -20,6 +23,7 @@ last10 = engine.select("SELECT * FROM Measure ORDER BY id DESC LIMIT 10")
 
 #¿Cómo cerrar la conexión con la base de datos?
 
+"""
 result1 = engine.select("SELECT * FROM CountMonth2020")
 for month,count in result1:
     print("Registro: %s, %s" % (month,count))
@@ -35,3 +39,8 @@ if(engine.connectionCheck()):
     print("Still connected")
 else:
     print("Disconnected")
+"""
+
+date = dataGenerator.DataGenerator(fileName = "data.sql")
+
+date.generate(100)
